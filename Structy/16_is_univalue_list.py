1b248d0ada2f2e2ba474efee7e22831e3b5139ed
Sam Sumiya <samsan@Sams-Mac-Studio.local>
2022-10-19 10:18:13 -0700

    added second recursive approach with an additional arg

--- Structy/16_is_univalue_list.py
+++ Structy/16_is_univalue_list.py
@@ -14,12 +14,19 @@
         ** the last node will be compare to None ** 
         make sure the head.next does not equal to None
 
-    approach: recursion 
+    approach: recursion 1
         basecaes: head.next is None then we can return True
 
         get the value of the current node and next node
         compare it 
         if nothing found: we can move onto the next node
+    
+    approach: recursion 2
+        base case would be when the head reaches the end
+        additionally we will add a prev_val as an arg
+        so we can check if prev_val is None or prev_val equals to the current head val or not
+        if it does, we will just move to the next node
+        until the if statement returns false
 '''
 
 
@@ -33,7 +40,7 @@ def is_univalue_list_iter(head):
     return True
 
 
-def is_univalue_list_recur(head):
+def is_univalue_list_recur_b(head):
     if head.next is None:
         return True
 
@@ -41,4 +48,16 @@ def is_univalue_list_recur(head):
     if head.val != head.next.val:
         return False
 
-    return is_univalue_list_recur(next_node)
+    return is_univalue_list_recur_b(next_node)
+
+
+def is_univalue_list_recur(head, prev_val = None): 
+    if head is None: return True
+
+    if prev_val == None or head.val == prev_val: 
+        prev_val = head.val
+        return is_univalue_list_recur(head, prev_val)
+    else: 
+        return False
+    
+    
