82b1f90b9cc1278202cbcd27c948aa65e127bd79
Sam Sumiya <samsan@Sams-Mac-Studio.local>
2022-10-21 09:27:41 -0700

    added grid travel

--- Dynamic Programming/02_grid_traveler.py
+++ Dynamic Programming/02_grid_traveler.py
@@ -8,14 +8,36 @@
 '''
 
 
-def grid_traveler(m, n):
-    pass 
+def grid_traveler(m, n, memo={}):
+    key = str(m) + ',' + str(n)
 
+    if key in memo: 
+        return memo[key]
 
+    if m == 0 or n == 0:
+        return 0
+    if m == 1 and n == 1:
+        return 1
 
+    memo[key] = grid_traveler(m - 1, n) + grid_traveler(m, n - 1)
+    return memo[key]
 
-
-m = 2
-n = 3
+m = 18
+n = 18
 r = grid_traveler(m, n)
 print(r)
+
+
+'''
+ if grid_traveler(m - 1, n, memo) in memo:
+        return memo[grid_traveler(m - 1, n, memo)]
+    if grid_traveler(m, n - 1, memo) in memo: 
+        return memo[grid_traveler(m, n - 1, memo)]
+    else:
+        if m == 0 or n == 0: return 0 
+        if m == 1 and n == 1: return 1
+
+
+
+        return memo[grid_traveler(m - 1, n, memo)] + memo[grid_traveler(m, n - 1, memo)]
+'''
