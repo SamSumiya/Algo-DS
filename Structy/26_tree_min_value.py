b66213c16505f3c7042890c0d8637315df9caf1d
Sam Sumiya <samsan@Sams-Mac-Studio.local>
2022-10-24 15:28:00 -0700

    added recursive approach and solution

--- Structy/26_tree_min_value.py
+++ Structy/26_tree_min_value.py
@@ -10,6 +10,12 @@
             if there is a value that is greater than the min value
             update it to that value
     
+    approach: depth first recursion
+        at the end, we return a float('inf')
+        so our job is to hit the base case for each branch and return the smaller nunber
+        so we should go left and right and each branch will return a number
+        at the end, we should get the left and right branchs' smallest number
+    
 '''
 
 
@@ -27,3 +33,13 @@ def tree_min_value_df_iter(root, min_val=float('inf')):
             stack.append(current.right)
 
     return min_val
+
+
+def tree_min_value_dp_recur(root):
+    if root is None:
+        return float('inf')
+
+    left = tree_min_value_dp_recur(root.left)
+    right = tree_min_value_dp_recur(root.right)
+
+    return min(root.val, left,  right)
