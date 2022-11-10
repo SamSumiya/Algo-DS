9df99f17ceb61effe439e2258b0f1015693fb910
Sam Sumiya <samsan@Sams-Mac-Studio.hsd1.ca.comcast.net>
2022-11-09 19:51:34 -0800

    added 141 linked list cycle

---
+++ leetcode/141_linked_list_cycle.py
@@ -0,0 +1,27 @@
+'''
+141. Linked List Cycle
+
+Approach: Time Complexity O(n) and Space Complexity O(n)
+
+    use node as a key for hashmap
+    if there is a same key(node) at any time, we know that we can return true
+    else we will return False
+
+'''
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        hash = {} 
+
+        while head: 
+
+            if head not in hash: 
+                hash[head] = head
+            else: 
+                return True
+            head = head.next 
+        return False
