21d6c190c0f674d1cfa3051853ea484a5a9250af
Sam Sumiya <samsan@Sams-Mac-Studio.local>
2022-11-09 21:26:20 -0800

    added detect cycle with extra space

--- LeetCode75/7_linked_list_cycle_2.py
+++ LeetCode75/7_linked_list_cycle_2.py
@@ -1,6 +1,25 @@
 '''
 142. Linked List Cycle II
 
+
+
 '''
 
 
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        my_list = set()
+        curr = head
+
+        while curr: 
+            if curr not in my_list:
+                my_list.add(curr)
+            else: 
+                return curr
+            curr = curr.next
+
+        return None
