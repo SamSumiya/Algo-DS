14651dacb15d4b3cbea9c432358125ea45030bbd
Sam Sumiya <samsan@Sams-Mac-Studio.hsd1.ca.comcast.net>
2022-11-12 10:32:32 -0800

    added O(1) space complexity solution to linked list cycle 2

--- LeetCode75/8_linked_list_cycle_2.py
+++ LeetCode75/8_linked_list_cycle_2.py
@@ -7,7 +7,7 @@
 
 
 class Solution(object):
-    def detectCycle(self, head):
+    def detectCycle_1(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
@@ -15,11 +15,39 @@ class Solution(object):
         my_list = set()
         curr = head
 
-        while curr: 
+        while curr:
             if curr not in my_list:
                 my_list.add(curr)
-            else: 
+            else:
                 return curr
             curr = curr.next
 
         return None
+
+    def detectCycle_2(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None:
+            return
+
+        slow = fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+            if slow == fast:
+                break
+
+        if not fast or not fast.next:
+            return
+
+        slow2 = head
+
+        while slow.next:
+            if slow == slow2:
+                return slow
+            slow = slow.next
+            slow2 = slow2.next
