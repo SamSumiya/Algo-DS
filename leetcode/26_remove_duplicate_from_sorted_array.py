f5b92b74d7c4ac722da0b75ffd66859d00114feb
Sam Sumiya <samsan@Sams-Mac-Studio.hsd1.ca.comcast.net>
2022-11-12 09:03:37 -0800

    added the approach

--- leetcode/26_remove_duplicate_from_sorted_array.py
+++ leetcode/26_remove_duplicate_from_sorted_array.py
@@ -2,6 +2,24 @@
     26. Remove Duplicates from Sorted Array
 
 
+Approach: 
+    Since its a sorted list
+    [1,1,2,2,3,5]
+
+    we will need to make the list look like this
+    [1,2,3,5,_,_]
+
+    We will loop through the list and find if the current val is not equal to the next
+    then we can update the current one to next one
+    so we get [1,2,2,2,3,5]
+    then next time we will update it to 
+    [1,2,3,2,3,5]
+    and 
+    [1,2,3,5,3,5]
+
+    It looks like we will just need to update from the first one to the last unique element
+    so we can use an index to track from 0 to the last one
+
 '''
 
 
@@ -17,7 +35,16 @@ class Solution(object):
         while i < len(nums)-1:
             if nums[i] != nums[i+1]:
                 nums[left] = nums[i+1]
+                print(nums)
                 left += 1
-                i += 1
+            i += 1
 
         return left
+
+
+input = [1,1,2,2,3,5]
+su = Solution()
+
+r = su.removeDuplicates(input)
+
+print(r)
