c857499952b6f8bb8c57aad55435bcc8f127d891
Sam Sumiya <samsan@Sams-Mac-Studio.hsd1.ca.comcast.net>
2022-11-21 21:29:45 -0800

    added solution for merge

--- leetcode/88_merge_sorted_arryay.py
+++ leetcode/88_merge_sorted_arryay.py
@@ -4,10 +4,6 @@
 '''
 
 
-
-
-
-
 from typing import List
 
 
@@ -16,4 +12,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        
+        last_index = m + n - 1
+        # m = m - 1
+        # n = n - 1
+
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[last_index] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[last_index] = nums2[n-1]
+                n -= 1
+
+            last_index -= 1
+
+        while n > 0:
+            nums1[last_index] = nums2[n-1]
+            n, last_index = n - 1, last_index - 1
